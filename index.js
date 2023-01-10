ebb154c99cedc5a544995559c7c9cf6cd3b2f64e
Eli <eli@landt.co>
2023-01-10 15:51:23 +0100

    test

--- index.js
+++
@@ -1,9 +0,0 @@
-const jsonFile = require('./data.json');
-const http = require('http');
-
-const app = http.createServer(function(req,res){
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(jsonFile));
-});
-
-app.listen(3000);
---
+++ netlify/functions/index.js
@@ -0,0 +1,16 @@
+const jsonFile = require('./../../data.json');
+
+exports.handler = async (event, context) => {
+ 
+ 
+	const response = JSON.stringify(jsonFile)
+ 
+	return {
+		statusCode: 200,
+		body: response,
+		headers: {
+			'Content-Type': 'application/json; charset=utf-8',
+			'Access-Control-Allow-Origin': '*',
+		},
+	}
+}
